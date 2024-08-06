e6223cc44bc181d3eab87deca6ac1681a641c8e2
Maleksk32 <167688315+Maleksk32@users.noreply.github.com>
2024-08-06 21:46:12 +0300

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -28,19 +28,20 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
+𝐍𝐚𝐦𝐞       : 𝐌𝐀𝐋𝐄𝐊 𝐑𝐀𝐉 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : ꔹꕥꔹꕥꔹꕥꘈꔹꙮꔹꘈꕥꔹꕥꔹꕥꔹ
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐓𝐚𝐧𝐠𝐚𝐢𝐥, 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐌𝐨𝐲𝐦𝐨𝐧𝐬𝐢𝐧𝐡, 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐈𝐧𝐝𝐢𝐚, 𝐌𝐮𝐫𝐬𝐡𝐢𝐝𝐚𝐛𝐚𝐝 
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐒𝐚𝐮𝐝𝐢 𝐀𝐫𝐚𝐛 , 𝐀𝐥 𝐐𝐚𝐬𝐬𝐢𝐦, 
+𝐁𝐮𝐫𝐚𝐲𝐝𝐚𝐡 
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
-𝐀𝐠𝐞           : 𝟏𝟖+
+𝐀𝐠𝐞           : 𝟐𝟐+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
-𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mohammadnayan447@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801615298449
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/MOHAMMADNAYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.xnxx.com169`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐖𝐨𝐫𝐤        : 𝐃𝐫𝐢𝐯𝐞𝐫 
+𝐆𝐦𝐚𝐢𝐥       : 𝐢𝐚𝐦𝐦𝐚𝐥𝐞𝐤𝐬𝐤𝟑𝟒𝟒@𝐠𝐦𝐚𝐢𝐥.𝐜𝐨𝐦 
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: 𝐰𝐚.𝐦𝐞/+ 𝟗𝟔𝟔𝟎𝟓𝟕𝟑𝟒𝟏𝟖𝟕𝟗𝟖 
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : 𝐌𝐀𝐋𝐄𝐊 𝐑𝐀𝐉 
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/malekskroy88`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
